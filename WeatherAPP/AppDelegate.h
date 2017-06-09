e3674b0b17a2f209f01ef0ec273475600a7333f3
Rodrigo <rodrigo@MacMinis-Mac-mini.local>
2017-06-09 14:05:26 -0300

    Initial Commit

---
+++ WeatherAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherAPP
+//
+//  Created by Rodrigo on 09/06/17.
+//  Copyright © 2017 Rodrigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
